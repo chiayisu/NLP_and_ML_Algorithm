2309392f8140e18787f55f97c6de82014d7c88de
IanSu <iansu1023@gmail.com>
2020-08-05 13:54:52 +0800

    Add Time RNN

--- Deep_Learning/RNN.py
+++ Deep_Learning/RNN.py
@@ -45,11 +45,39 @@ class RNN:
         self.grads.append(db)
         dx = np.dot(dtanh, self.W_x.T)
         return d_h_minus_1, dx
-        
 
+class Time_RNN:
+    def __init__(self, input_size, hidden_size, batch_size, sequence_length):
+        self.rnn = []
+        self.h_t_list = []
+        for _ in range(sequence_length):
+            self.rnn.append(RNN(input_size, hidden_size, batch_size))
+
+    def forward(self, x, h_t_minus_1):
+        self.h_t_list = []
+        for index, x_t in enumerate(x):
+            h_t_minus_1 = self.rnn[index].forward(x_t, h_t_minus_1)
+            self.h_t_list.append(h_t_minus_1)
+        return self.h_t_list
+
+    def backward(self, dnext):
+        for reversed_rnn in reversed(self.rnn):
+            dnext, dx = reversed_rnn.backward(dnext)
+        return dnext, dx 
+
+
+##h_t_minus_1 = np.random.randn(2, 3)
+##x = np.array([[1, 2, 3], [1, 2, 3]])
+##rnn = RNN(3, 3, 2)
+##y = rnn.forward(x, h_t_minus_1)
+##grads = rnn.backward(y)
 
 h_t_minus_1 = np.random.randn(2, 3)
-x = np.array([[1, 2, 3], [1, 2, 3]])
-rnn = RNN(3, 3, 2)
-y = rnn.forward(x, h_t_minus_1)
-grads = rnn.backward(h_t_minus_1)
+x = np.array([[[1, 2, 3], [1, 2, 3]]])
+rnn = Time_RNN(3, 3, 2, 1)
+h_last = rnn.forward(x, h_t_minus_1)
+print(h_last)
+rnn.backward(h_last)
+
+
+
