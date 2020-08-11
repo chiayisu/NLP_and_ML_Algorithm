74de5862aa04c801a8fbc549436fe0245bd386e4
IanSu <iansu1023@gmail.com>
2020-08-11 10:27:04 +0800

    Fix bug on negative sampling

--- Word_Vector/Word_Vec_with_Negative_Sampling.py
+++ Word_Vector/Word_Vec_with_Negative_Sampling.py
@@ -5,37 +5,38 @@ def Sigmoid(x):
     out = 1 / (1 + np.exp(-x))
     return 1 / (1 + np.exp(-x))
 
-def Softmax(x):
-    denominator = np.sum(np.exp(x), axis = 1)
-    denominator = denominator[:, np.newaxis]
-    return np.exp(x) / np.exp(denominator)
-
-def CrossEntropyLoss(y, y_hat):
-    N = y.shape[0]
-    log_probability = np.log(y_hat)
-    total_loss = np.sum(y * log_probability)
-    total_loss = total_loss * -1 
-    return total_loss / N
-
-class LossFunction:
-    def __init__(self):
-        self.params = []
-        self.y_hat = None
-        self.y = None
-    
-    def forward(self, x, y):
-        y_hat = Sigmoid(x)
-        self.y = y
-        self.y_hat = y_hat
-        loss = CrossEntropyLoss(y, y_hat)
-        return loss
-
-    def backward(self, dout=1):
-        dimension = self.y_hat.shape[0]
-        dx = self.y - self.y_hat
-        dx = dout * dx
-        dx /= dimension
-        return dx
+##def Softmax(x):
+##    denominator = np.sum(np.exp(x), axis = 1)
+##    denominator = denominator[:, np.newaxis]
+##    return np.exp(x) / np.exp(denominator)
+##
+##def CrossEntropyLoss(y, y_hat):
+##    N = y.shape[0]
+##    pos_log_probability = np.log(y_hat[0])
+##    total_loss = np.sum(pos_log_probability)
+##    total_loss += np.log(1-y_hat[1:])
+##    total_loss = total_loss * -1 
+##    return total_loss / N
+##
+##class LossFunction:
+##    def __init__(self):
+##        self.params = []
+##        self.y_hat = None
+##        self.y = None
+##    
+##    def forward(self, x, y):
+##        y_hat = Sigmoid(x)
+##        self.y = y
+##        self.y_hat = y_hat
+##        loss = CrossEntropyLoss(y, y_hat)
+##        return loss
+##
+##    def backward(self, dout=1):
+##        dimension = self.y_hat.shape[0]
+##        dx = self.y - self.y_hat
+##        dx = dout * dx
+##        dx /= dimension
+##        return dx
 
 class SGD:
     def __init__(self, lr = 0.01):
@@ -48,49 +49,62 @@ class SGD:
 class CBOW_with_Negative_Sampling:
     def __init__(self, input_dim, hidden_dim, output_dim):
         self.grads, self.params = [], []
-        self.output_dim = output_dim
+        self.hidden_dim = hidden_dim
         self.Win = np.random.randn(input_dim, hidden_dim)
         self.Wout = np.random.randn(hidden_dim, output_dim)
         self.params.append(self.Wout)
         self.params.append(self.Win)
-        self.loss_layer = LossFunction()
         self.center = None
-        self.hidden_0 = None
+        self.center_index = None
+        self.pos_output = None
+        self.neg_output = None
+        self.h = None
+        self.negative_sample = None
+        self.neg_list = []
         
-    def forward(self, context, center_word, negtive_Sample):
-        self.context = context
-        hidden_0 = np.dot(context, self.Win)
-        self.hidden_0 = hidden_0
-        hidden_1 = np.dot(hidden_0, self.Wout)
-        loss = self.loss_layer.forward(hidden_1, center_word)
-
-##     Negtive Sampling
+    def forward(self, center_word, Negative_Sample):
+        h = np.dot(center_word, self.Win)
+        self.h  = h
+        self.center_index = np.argmax(center_word)
+        hidden_1 = np.dot(self.Wout.T[self.center_index].reshape(1,self.hidden_dim), self.h.T)
+        self.pos_output = Sigmoid(hidden_1)
+        loss = np.log(self.pos_output) * (-1)
+        
+##        Negtive Sampling
         Wout_T = self.Wout.T
-        U = np.dot(negtive_Sample, Wout_T)
-        negative = np.dot(U, self.hidden_0[0].T)
-        negative = negative * (-1)
-        loss += self.loss_layer.forward(negative, negtive_Sample)
-        return loss
+        self.negative_sample = Negative_Sample
+        for negative_sample in Negative_Sample:
+            negative = np.dot(Wout_T[negative_sample].reshape(1, self.hidden_dim), self.h.T)
+            negative = negative * (-1)
+            self.neg_output = Sigmoid(negative)
+            self.neg_list.append(self.neg_output)
+            loss += np.log(self.neg_output) * (-1)
+        return loss[0]
 
-    def backward(self, dout = 1):
+    def backward(self):
         self.grads = []
-        dout = self.loss_layer.backward()
-        dWout = np.dot(self.hidden_0.T, dout)
-        dout = np.dot(dout, self.Wout.T)
-        dWin = np.dot(self.context.T, dout)
-        dout = np.dot(dout, self.Win.T)
-        self.grads.append(dWout)
+        dWout = np.zeros_like(self.Wout.T)
+        dWout[self.center_index] = (self.pos_output - 1) * self.h
+        for index, negative_sample in enumerate(self.negative_sample):
+            dWout[negative_sample] = self.neg_list[index] * self.h
+        self.grads.append(dWout.T)
+
+        dWin = np.zeros_like(self.Win)
+
+        temp = (self.pos_output - 1) * dWin[self.center_index].reshape(1, self.hidden_dim) 
+        for index, negative_sample in enumerate(self.negative_sample):
+            temp += self.neg_list[index] * self.h
+        dWin[self.center_index] = temp
         self.grads.append(dWin)
-        return dout
 
 context = np.array([[0, 1, 0, 1, 0]])
-negtive_Sample = np.array([[1, 0, 0, 0, 0]])
+negtive_Sample = np.array([0, 2])
 center_word = np.array([[0, 0, 0, 0, 1]])
 Word_Vector = CBOW_with_Negative_Sampling(5, 3, 5)
 optimizer = SGD(0.0001)
 loss_list = []
 for epoch in range(10):
-    loss = Word_Vector.forward(context, center_word, negtive_Sample)
+    loss = Word_Vector.forward(center_word, negtive_Sample)
     loss_list.append(loss)
     dout = Word_Vector.backward()
     optimizer.update(Word_Vector.params, Word_Vector.grads)
