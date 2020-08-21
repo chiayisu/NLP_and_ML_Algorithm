d65058889345b5e4cc7a18ca918ef452d0a27622
IanSu <iansu1023@gmail.com>
2020-08-21 14:27:11 +0800

    Fix Bugs on RNN & RNNLM

--- Deep_Learning/RNN.py
+++ Deep_Learning/RNN.py
@@ -49,7 +49,6 @@ class RNN:
 class Time_RNN:
     def __init__(self, W_h, W_x, b, sequence_length):
         self.params = [W_h, W_x, b]
-        self.grads = [np.zeros_like(W_h), np.zeros_like(W_x), np.zeros_like(b)]
         self.rnn = []
         self.h_t_list = []
         for _ in range(sequence_length):
@@ -67,7 +66,9 @@ class Time_RNN:
         self.grads = [np.zeros_like(W_h), np.zeros_like(W_x), np.zeros_like(b)]
         for reversed_rnn in reversed(self.rnn):
             dnext, dx = reversed_rnn.backward(dnext)
-            self.grads += reversed_rnn.grads
+            self.grads[0] += reversed_rnn.grads[0]
+            self.grads[1] += reversed_rnn.grads[1]
+            self.grads[2] += reversed_rnn.grads[2]
         return dnext, dx 
 
 input_size = 3
@@ -84,10 +85,9 @@ h_t_minus_1 = np.random.randn(2, 3)
 ##y = rnn.forward(x, h_t_minus_1)
 ##grads = rnn.backward(y)
 
-x = np.array([[[1, 2, 3], [1, 2, 3]]])
-rnn = Time_RNN(W_h, W_x, b, 1)
+x = np.array([[[1, 2, 3], [1, 2, 3]], [[1, 2, 3], [1, 2, 3]]])
+rnn = Time_RNN(W_h, W_x, b, 2)
 h_last = rnn.forward(x, h_t_minus_1)
 rnn.backward(1)
-
 
 
--- Deep_Learning/RNNLM.py
+++ Deep_Learning/RNNLM.py
@@ -15,7 +15,6 @@ def CrossEntropyLoss(y, y_hat):
     return total_loss / N
 
 class Embedding_Layer:
-
     def __init__(self, embbeded_matrix):
         self.embedded_weight = embbeded_matrix
         self.index = None
@@ -73,40 +72,50 @@ class SoftmaxwithLoss:
         return dx
 
 class RNNLM:
-    def __init__(self, input_size, hidden_size, batch_size, vocabuary_size, embedding_matrix):
-        self.params = []
+    def __init__(self, W_h, W_x, b, vocabuary_size, embedding_matrix):
+        self.params = [W_h, W_x, b]
         self.grads = []
+        hidden_size = W_h.shape[0]
         self.RNNLM = [Embedding_Layer(embedding_matrix),
-                      RNN.RNN(input_size, hidden_size, batch_size),
+                      RNN.RNN(W_h, W_x, b),
                       Affine(hidden_size, vocabuary_size)
                       ]
         self.loss_layer = SoftmaxwithLoss()
-        for layer in reversed(self.RNNLM):
-            self.params.append(layer.params)
+            
     def forward(self, input_word, h_t_minus_one, y):
         word_embbeded = self.RNNLM[0].forward(input_word)
+        self.params.append(self.RNNLM[0].params)
         h_t = self.RNNLM[1].forward(word_embbeded, h_t_minus_one)
         affine = self.RNNLM[2].forward(h_t)
+        self.params.append(self.RNNLM[2].params[0])
+        self.params.append(self.RNNLM[2].params[1])
         y_hat, loss = self.loss_layer.forward(affine, y)
         predicted_word = np.argmax(y_hat, axis=1)
         return predicted_word, h_t, loss
     
     def backward(self, dhnext, dout = 1):
-        self.grads = []
-        dx = self.loss_layer.backward()
-        dx = self.RNNLM[2].backward(dx)
-        dhnext, dx = self.RNNLM[1].backward(dhnext + dx)
-        dx = self.RNNLM[0].backward(dx)
-        for layer in reversed(self.RNNLM):
-            self.grads.append(layer.grads)
+        W_h, W_x, b = self.params[0], self.params[1], self.params[2]
+        self.grads = [np.zeros_like(W_h), np.zeros_like(W_x), np.zeros_like(b)]
+        dout = self.loss_layer.backward(dout)
+        dout = self.RNNLM[2].backward(dout)
+        dhnext, dout = self.RNNLM[1].backward(dhnext + dout)
+        dout = self.RNNLM[0].backward(dout)
+        self.grads[0] += self.RNNLM[1].grads[0]
+        self.grads[1] += self.RNNLM[1].grads[1]
+        self.grads[2] += self.RNNLM[1].grads[2]
+        self.grads.append(self.RNNLM[0].grads)
+        self.grads.append(self.RNNLM[2].grads[0])
+        self.grads.append(self.RNNLM[2].grads[1])
         return dhnext
 
 class Time_RNNLM:
-    def __init__(self, input_size, hidden_size, batch_size, vocabuary_size, embedding_matrix, sequence_length):
+    def __init__(self, W_h, W_x, b, vocabuary_size, embedding_matrix, sequence_length):
         self.rnnlm = []
         self.predicted_word_list = []
+        self.params = [W_h, W_x, b]
+        self.grads = []
         for _ in range(sequence_length):
-            self.rnnlm.append(RNNLM(input_size, hidden_size, batch_size, vocabuary_size, embedding_matrix))
+            self.rnnlm.append(RNNLM(W_h, W_x, b, vocabuary_size, embedding_matrix))
 
     def forward(self, x, y, h_t_minus_1):
         LOSS = 0
@@ -115,21 +124,47 @@ class Time_RNNLM:
         for index, y_t in enumerate(y):
             y_t = np.array([y_t])
             predicted_word, h_t_minus_1, loss = self.rnnlm[index].forward(predicted_word, h_t_minus_1, y_t)
+            self.params.append(self.rnnlm[index].params[3])
+            self.params.append(self.rnnlm[index].params[4])
+            self.params.append(self.rnnlm[index].params[5])
             LOSS += loss
             self.predicted_word_list.append(predicted_word)
         return self.predicted_word_list, LOSS
 
     def backward(self):
         dhnext = 0
+        Wx, Wh, b = self.params[0], self.params[1], self.params[2]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
         for reversed_rnnlm in reversed(self.rnnlm):
             dhnext = reversed_rnnlm.backward(dhnext, 1)
+            self.grads[0] += reversed_rnnlm.grads[0]
+            self.grads[1] += reversed_rnnlm.grads[1]
+            self.grads[2] += reversed_rnnlm.grads[2]
+        for rnnlm in self.rnnlm:
+            self.grads.append(rnnlm.grads[3])
+            self.grads.append(rnnlm.grads[4])
+            self.grads.append(rnnlm.grads[5])
 
+input_size = 2
+hidden_size = 3
+batch_size = 1
 
+W_h = np.random.randn(hidden_size, hidden_size)
+W_x = np.random.randn(input_size, hidden_size)
+b = np.random.randn(batch_size, hidden_size)
 h_t_minus_one = np.random.randn(1, 3)
+
 a = np.array([[0, 1], [1, 2], [2, 3]])
 y = np.array([[0, 0, 1], [0, 1, 0], [0, 0, 1]])
 input_word = np.array([0])
-rnn = Time_RNNLM(2, 3, 1, 3, a, 3)
+rnn = Time_RNNLM(W_h, W_x, b, 3, a, 3)
 predicted_word, loss = rnn.forward(input_word, y, h_t_minus_one)
 rnn.backward()
+print(len(rnn.grads))
+
+print(len(rnn.params))
+
+
+
+
 
